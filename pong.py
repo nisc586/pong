ad1253125befb192ea6910cdd44f38c2e7130f67
nisc586 <49026680+nisc586@users.noreply.github.com>
2022-12-07 10:43:28 +0100

    Implement scoreboard

--- pong.py
+++ pong.py
@@ -12,6 +12,9 @@ BAT_SPEED = 9
 BALL_SIZE = (20, 20)
 BALL_SPEED = 7
 
+TEXT_SIZE = 40
+TEXT_POSITION = (MIDDLE[0], 10)
+
 RGB_BLACK = (0, 0, 0)
 RGB_WHITE = (255, 255, 255)
 
@@ -30,6 +33,7 @@ class Bat(pg.sprite.Sprite):
         if down and not self.rect.bottom >= SCREEN_SIZE[1]:
             self.rect.move_ip(0, self.speed)
 
+
 class Ball(pg.sprite.Sprite):
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
@@ -40,20 +44,22 @@ class Ball(pg.sprite.Sprite):
         self.rect = pg.Rect(MIDDLE,(self.image.get_width(),self.image.get_height()))
 
         self.reset()
-
-    
+ 
     def move(self):
         if self.rect.top <= 0 or self.rect.bottom >= SCREEN_SIZE[1]:
             self.movement.y = -self.movement.y
-        if self.rect.left <= 0 or self.rect.right >= SCREEN_SIZE[0]:
-            self.set_random_direction()
-            self.set_position(MIDDLE)
         
         self.rect.move_ip(self.movement.x, self.movement.y)
     
     def collide(self, bat):
         if self.rect.colliderect(bat.rect):
             self.movement.x = - self.movement.x
+
+    def is_outside_screen_left(self):
+        return self.rect.left <= 0
+    
+    def is_outside_screen_right(self):
+        return self.rect.right >= SCREEN_SIZE[0]
 
     def reset(self):
         self.set_position(MIDDLE)
@@ -68,8 +74,26 @@ class Ball(pg.sprite.Sprite):
         self.movement *= choice([-1, 1])
         self.movement.rotate_ip(randint(-60, 60))
 
+
+class Scoreboard():
+    def __init__(self):
+        self.score_left = self.score_right = 0
+        self.font = pg.font.SysFont("corbel", TEXT_SIZE, bold=True)
+    
+    def get_text(self):
+        text = f"{self.score_left} : {self.score_right}"
+        return self.font, text
+    
+    def point_left(self):
+        self.score_left += 1
+    
+    def point_right(self):
+        self.score_right += 1
+        
+
 def main():
     pg.init()
+    pg.font.init()
     screen = pg.display.set_mode(SCREEN_SIZE, pg.SCALED)
     pg.display.set_caption(GAME_TITLE)
     pg.mouse.set_visible(False)
@@ -83,6 +107,7 @@ def main():
     ball = Ball()
     bat_left = Bat((MARGIN, MIDDLE[1]))
     bat_right = Bat((SCREEN_SIZE[0] - MARGIN - BAT_SIZE[0], MIDDLE[1]))
+    scoreboard = Scoreboard()
 
     allsprites = pg.sprite.Group(ball, bat_left, bat_right)
 
@@ -107,6 +132,13 @@ def main():
         
         # Logical updates here
         #---------------------
+        if ball.is_outside_screen_left():
+            scoreboard.point_left()
+            ball.reset()
+        elif ball.is_outside_screen_right():
+            scoreboard.point_right()
+            ball.reset()
+
         ball.collide(bat_left)
         ball.collide(bat_right)
         ball.move()
@@ -114,9 +146,16 @@ def main():
         # Render graphics here
         #---------------------
         screen.blit(background, (0, 0))
+
+        font, text = scoreboard.get_text()
+        text_surface = font.render(text, True, RGB_WHITE)
+        textpos = text_surface.get_rect(centerx=TEXT_POSITION[0], y=TEXT_POSITION[1])
+        screen.blit(text_surface, textpos)
+
         allsprites.draw(screen)
         pg.display.flip()
         clock.tick(60)
+
 
 if __name__ == "__main__":
     main()
