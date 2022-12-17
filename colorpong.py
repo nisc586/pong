c60c4d45c58d8636705a739ecb16b6bec568e349
nisc586 <49026680+nisc586@users.noreply.github.com>
2022-12-17 15:30:19 +0100

    Add triangle tip to Nozzle sprite

--- colorpong.py
+++ colorpong.py
@@ -1,4 +1,5 @@
 import pygame as pg
+
 import random
 from collections import namedtuple
 
@@ -12,7 +13,10 @@ SCREEN_SIZE = Size(1200, 900)
 MIDDLE = Position(600, 450)
 MARGIN = 10
 
-NOSSLE_POSITION = Position(MIDDLE.x, MARGIN)
+NOZZLE_POSITION = Position(MIDDLE.x, SCREEN_SIZE.height - MARGIN)
+NOZZLE_SIZE = Size(20, 20)
+NOZZLE_RADIUS = 25
+NOZZLE_RADIAL_SPEED = 3
 
 BACKGROUND_COLOR = pg.Color("black")
 
@@ -20,6 +24,7 @@ BACKGROUND_COLOR = pg.Color("black")
 BALL_SIZE = Size(20, 20)
 BALL_COLORS = [pg.Color(s) for s in ("red", "blue", "green", "yellow", "orange", "purple")]
 DEFAULT_BALL_SPEED = 7
+
 
 class Ball(pg.sprite.Sprite):
     def __init__(self, position, color=None):
@@ -33,8 +38,22 @@ class Ball(pg.sprite.Sprite):
         self.rect = pg.draw.circle(surface=self.image, color=self.color, center=(BALL_SIZE.width // 2, BALL_SIZE.height // 2), radius=BALL_SIZE.width // 2)
         self.rect.center = position
 
-class Nossle:
-    pass
+
+class Nozzle(pg.sprite.Sprite):
+    def __init__(self, ball):
+        pg.sprite.Sprite.__init__(self)
+        self.ball = ball
+
+        self.image = pg.Surface(NOZZLE_SIZE)
+        bl = Position(0, NOZZLE_SIZE.height)
+        br = Position(NOZZLE_SIZE.width, NOZZLE_SIZE.height)
+        tip = Position(NOZZLE_SIZE.width // 2, 0)
+        self.rect = pg.draw.polygon(self.image, pg.Color("white"), [bl, br, tip])
+        self.rect.center = NOZZLE_POSITION
+
+    def turn(self, left=False, right=False):
+        pass
+
 
 
 def main():
@@ -46,6 +65,8 @@ def main():
 
     positions_x = [100, 150, 200, 250, 300,  350]
     balls = pg.sprite.Group([Ball((pos, MIDDLE.y), col) for pos, col in zip(positions_x, BALL_COLORS)])
+    nozzle = Nozzle(None)
+    playersprites = pg.sprite.Group(nozzle)
 
     while True:
         for event in pg.event.get():
@@ -58,6 +79,7 @@ def main():
         #---------------------
         screen.blit(background, (0, 0))
         balls.draw(screen)
+        playersprites.draw(screen)
         pg.display.flip()
 
 
